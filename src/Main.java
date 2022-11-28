ba22332a548666dbff244c0cffc1903d697bac90
gmeditsk <gmeditsk@csd.auth.gr>
2022-11-28 16:34:53 +0200

    misc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.io.*;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
@@ -15,22 +18,22 @@ public class Main {
             e.printStackTrace();
         }
 
-        // open and append
+//        // open and append
         try (BufferedWriter buffer = new BufferedWriter(new FileWriter(outFile, true))) {
             buffer.write("test3");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //open for read
+//
+//        //open for read
         try (BufferedReader buffer = new BufferedReader(new FileReader(outFile))) {
             String line = buffer.readLine();
             System.out.println(line.length());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // read all lines
+//
+//       // read all lines
         try (BufferedReader buffer = new BufferedReader(new FileReader(outFile))) {
             String line = buffer.readLine();
             while (line != null) {
@@ -41,7 +44,8 @@ public class Main {
             e.printStackTrace();
         }
 
-        //split lines
+
+//      //split lines
         try (BufferedReader buffer = new BufferedReader(new FileReader(outFile))) {
             String line = buffer.readLine();
             while (line != null) {
@@ -54,20 +58,20 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // read characters
+//
+//        // read characters
         try (BufferedReader buffer = new BufferedReader(new FileReader(outFile))) {
             int c = buffer.read();
             while (c != -1) {
                 char ch = (char) c;
-                System.out.println(ch);
+                System.out.println(c);
                 c = buffer.read();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // write binary
+//
+//       // write binary
         List<String> list = Arrays.asList("george", "maria");
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("files/binary.dat"))) {
             oos.writeObject(list);
@@ -75,15 +79,16 @@ public class Main {
             e.printStackTrace();
         }
 
-        //load binary
+//       //load binary
         try (ObjectInputStream oos = new ObjectInputStream(new FileInputStream("files/binary.dat"))) {
-            list = (List<String>) oos.readObject();
-            System.out.println(list);
+            List<String> list2;
+            list2 = (List<String>) oos.readObject();
+            list2.add("nick");
+            System.out.println(list2);
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
     }
-
 
 }
